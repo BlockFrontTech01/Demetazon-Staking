ccc54c0650a4b5d632536765c669c6470dd1f4b6
Jeff <148919566+jeff96655@users.noreply.github.com>
2023-12-02 09:32:44 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ const DemetazonStaking = () => {
   }, []);
 
   const onButton4Click = useCallback(() => {
-    window.open("https://blockfronttech.vercel.app");
+    window.open("https://jeffspace.vercel.app");
   }, []);
 
   useEffect(() => {
