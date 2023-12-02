261d1aee72138c973badd7c7c49ed0f48d10a41b
Jeff <148919566+jeff96655@users.noreply.github.com>
2023-12-02 09:31:51 +0100

    Update demetazon-staking-menu.js

--- components/demetazon-staking-menu.js
+++ components/demetazon-staking-menu.js
@@ -42,7 +42,7 @@ const DemetazonStakingMenu = () => {
   }, []);
 
   const onButton2Click = useCallback(() => {
-    window.open("https://blockfronttech.vercel.app");
+    window.open("https://jeffspace.vercel.app");
   }, []);
 
   return (
